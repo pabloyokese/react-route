bf332259911481b4d387ddda6fd5ac34b3df238c
pabloyokese <jpo_54@hotmail.com>
2019-03-30 12:24:26 +0800

    adding more components to practice

--- src/components/profiles.js
+++ src/components/profiles.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Card from '../hoc/card';
+import Auth from '../hoc/auth';
+
 const Profile = (props) =>{
     
     const redir = () => {
-        props.history.push('/')
+        // props.history.push('/')
     }
 
     return (
-        <div>
-            <Link to={
-                {
-                    pathname: `${props.match.url}/posts`
-                }
-            }>Take me to /profile/posts</Link>
-            {redir()}
-        </div>
+        <Auth>
+            <Card>
+                <Link to={
+                    {
+                        pathname: `${props.match.url}/posts`
+                    }
+                }>Take me to /profile/posts</Link>
+                {redir()}
+            </Card>
+        </Auth>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route,Link,NavLink, Switch } from 'react-router-dom';
+import {BrowserRouter, Route,NavLink, Switch } from 'react-router-dom';
 
 import Home from './components/home';
 import Posts from './components/posts';
 import Profile from './components/profiles';
 import PostItem from './components/post_item';
+import Life from './components/life';
+import Conditional from './components/conditional';
+import User from './components/user';
 
 const App = () =>{
     return (
@@ -20,12 +23,18 @@ const App = () =>{
                     <NavLink to={{
                         pathname:'/profile'
                     }}>Profile</NavLink><br/>
+                    <NavLink to="/life">Life Circles</NavLink><br/>
+                    <NavLink to="/conditional">Conditional</NavLink><br/>
+                    <NavLink to="/user">User</NavLink><br/>
                     <hr/>
                 </header>
                 <Switch>
                     <Route path="/posts/:id/:username" component={PostItem} />
                     <Route path="/profile" component={Profile}/>
                     <Route path="/posts" component={Posts} />
+                    <Route path="/life" exact component={Life} />
+                    <Route path="/conditional" exact component={Conditional} />
+                    <Route path="/user" exact component={User} />
                     <Route path="/" exact component={Home} />
                     <Route component={Posts} />
                 </Switch>
